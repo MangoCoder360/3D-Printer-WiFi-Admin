ebb1b343a6cb0015b478577c3d7ef6338f6eef3b
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2020-12-18 14:27:48 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import urllib2, easygui, time
 serverIP = easygui.enterbox("Please enter the IP address of your printer (eg. 000.000.00.00):","3D Printer WiFi Admin")
 serverIP = str(serverIP)
 serverIP = "http://" + serverIP
-chs = ["EXIT","Home","Dance","Unlock","Go to center"]
+chs = ["EXIT","Home","Dance","Go to center"]
 def main():
     ans = easygui.buttonbox("Select an action:","3D Printer WiFi Admin",chs)
     if ans == "Home":
@@ -11,13 +11,17 @@ def main():
         urllib2.urlopen(serverIP + "/set?code=G28%20Z")
         main()
     if ans == "Dance":
+        urllib2.urlopen(serverIP + "/set?code=M220%20S800")
         urllib2.urlopen(serverIP + "/set?code=G02%20X60Y60%20I0J-20.0")
         urllib2.urlopen(serverIP + "/set?code=G02%20X60Y60%20I0J-20.0")
         urllib2.urlopen(serverIP + "/set?code=G02%20X60Y60%20I0J-20.0")
         urllib2.urlopen(serverIP + "/set?code=G02%20X60Y60%20I0J-20.0")
         urllib2.urlopen(serverIP + "/set?code=G02%20X60Y60%20I0J-20.0")
+        urllib2.urlopen(serverIP + "/set?code=M220%20S180")
         main()
-
+    if ans == "Go to center":
+        urllib2.urlopen(serverIP + "/set?code=G0%20X50%20Y50%20Z50")
+        main()
 
 if __name__ == "__main__":
     main()
